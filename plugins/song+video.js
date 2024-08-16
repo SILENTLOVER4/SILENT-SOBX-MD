8aa11dc2527a5d56524d8617307d06d5479610ca
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-08-16 23:01:30 +0500

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,1 +1,19 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
 
+
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
