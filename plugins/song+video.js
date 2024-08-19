8a7bfbebc95685c8b2f798beef816f06131ed5a7
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-08-19 20:51:04 +0500

    Create song+video.js

---
+++ plugins/song+video.js
@@ -0,0 +1,48 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+
+
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Please give me url title")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc `
+🔥 *SILENT-SOBX-MD SONG DOWNLOADER* 🔥 
+
+title ${data.title}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+MADE BY SILENTLOVER432💙
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+    
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio message
+await conn.sendMessage(from,{audio: {url: downloadUrl},memetype:"audio/mpeg"},{quoted:mek});
+
+
+
+
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
