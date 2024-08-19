f9830c7115aa5d34e34a343ebdf801c3bfbc07fb
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-08-19 11:18:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ require("./plugins/" + plugin);
 console.log('Plugins installed successful ✅')
 console.log('Bot connected to whatsapp ✅')
 
-let up = `SILENT-SOBX-MD connected successful ✅\n\nPREFIX: ${.}`;
+let up = `SILENT-SOBX-MD connected successful ✅\n\nPREFIX: ${prefix}`;
 
 conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/3ef5623a33997cf948438.jpg` }, caption: up })
 
