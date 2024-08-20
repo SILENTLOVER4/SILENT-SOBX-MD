4207855683a1661a1b3c79bfd628a038ab182271
Ɗᴀʀᴋ Ɗєνιʟ 👑🇵🇰🖤 <umarofficial404@gmail.com>
2024-08-20 15:59:22 +0500

    Create ping.js

---
+++ plugins/ping.js
@@ -0,0 +1,26 @@
+const config = require('../config')
+let fs = require('fs')
+const { cmd, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+cmd({
+    pattern: "ping",
+    react: "📟",
+    alias: ["speed"],
+    desc: "Check bot\'s ping",
+    category: "main",
+    use: '.ping',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+var inital = new Date().getTime();
+let ping = await conn.sendMessage(from , { text: '𝙿𝙸𝙽𝙶'  }, { quoted: mek } )
+var final = new Date().getTime();
+await conn.sendMessage(from, { delete: ping.key })
+return await conn.sendMessage(from , { text: '*Pong*\n *' + (final - inital) + ' ms* '  }, { quoted: mek } )
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+})
+
