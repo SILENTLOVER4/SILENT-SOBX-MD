34353d05be562836ba90b24c213c55d019b4d5de
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-08-21 12:21:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ conn.ev.on('messages.upsert', async(mek) => {
 mek = mek.messages[0]
 if (!mek.message) return	
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-if (mek.key && mek.key.remoteJid === 'status@broadcast' && Config.AUTO_READ_STATUS === "true"){
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
 await conn.readMessages([mek.key])
 }
 const m = sms(conn, mek)
