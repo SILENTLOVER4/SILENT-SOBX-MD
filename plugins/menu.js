61186760fcba15eeda877138132e6d0221c9b9f4
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-08-22 19:38:06 +0500

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,13 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+
