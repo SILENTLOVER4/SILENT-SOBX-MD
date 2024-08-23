e8a1a52dd5381612cb3bfdab51a35ab1575c8889
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-08-23 07:51:33 +0500

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,61 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `.${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = `😼 *Hello ${pushname}*
+> *DOWNLOAD COMMANDS* 🔥
+
+${menu.download}
+
+> *MAIN COMMANDS* 🔥
+
+${menu.main}
+
+> *GROUP COMMANDS* 🔥
+
+${menu.group}
+
+> *OWNER COMMANDS* 🔥
+
+${menu.owner}
+
+> *CONVERT COMMANDS* 🔥
+
+${menu.convert}
+
+> *SEARCH COMMANDS* 🔥
+
+${menu.search}
+
+POWERD BY SILENTLOVER432👑
+`
+
+await conn.sendMessage(from,{text:madeMenu},{quoted.mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
