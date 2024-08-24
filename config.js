326c6203fbabcca463056ca3616296a293cd35ae
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-08-24 23:49:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg", 
-ALIVE_MSG: process.env.ALIVE_MSG || "ʜɪɪ ᴅᴇᴀʀ ɪ ᴀᴍ sɪʟᴇɴᴛ-sᴏʙx-ᴍᴅ ᴄʀᴇᴀᴛᴇᴅ ʙʏ `sɪʟᴇɴᴛ-ʟᴏᴠᴇʀ⁴³²` ɪᴀᴍ ᴏɴʟɪɴᴇ ɴᴏᴡ ᴡʜᴏ ᴄᴀɴ ɪ ʜᴇʟᴘ ʏᴏᴜ ᴛʏᴘᴇ ᴍᴇɴᴜ ᴛᴏ ᴄᴏᴍᴍᴀɴᴅs ʟɪsᴛ",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || 'true',
+MONOGODB: PROCESS.ENV.MONOGODB || "enter your Monogodbkey",
 };
