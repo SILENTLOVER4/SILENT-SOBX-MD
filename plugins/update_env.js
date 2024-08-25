9cd5619efa6fd7e00ff35e3a5edb9a76e58eef18
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-08-25 12:54:30 +0500

    Update update_env.js

--- plugins/update_env.js
+++ plugins/update_env.js
@@ -4,6 +4,7 @@ const { cmd } = require('../command');
 
 cmd({
     pattern: "update",
+    react: "🍂",
     alias: ["updateenv"],
     desc: "Check and update environment variables",
     category: "owner",
@@ -13,7 +14,7 @@ async (conn, mek, m, { from, q, reply, isOwner }) => {
     if (!isOwner) return;
 
     if (!q) {
-        return reply("🙇‍♂️ *Please provide the environment variable and its new value.* \n\nExample: `.update ALIVE_MSG: hello i am prabath kumara`");
+        return reply("🙇‍♂️ *Please provide the environment variable and its new value.* \n\nExample: `.update ALIVE_MSG: hello i am silent lover`");
     }
 
     // Find the position of the first colon or comma
