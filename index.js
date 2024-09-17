60201034735205be273ef047fae7351e328cdfed
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-17 10:56:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ async function connectToWA() {
 const connectDB = require('./lib/mongodb')
 connectDB();
 //==================================
+const {readEnv} = require('./lib/database')
+const config = await readEnv();
+const prefix = config.PREFIX
+//=================================
+        
 console.log("Connecting wa bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
