73ad543fe4772598f44fd9af83a1a756024a4b4e
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-17 14:23:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const PREFIX = ('.)
+const PREFIX = ('.')
 
 const ownerNumber = ['923096287432']
 
