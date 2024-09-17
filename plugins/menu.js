08c9e05469e1c60a16febbcf3152b53ffb115466
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-17 16:18:18 +0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -22,7 +22,7 @@ search: ''
 
 for (let i = 0; i < commands.length; i++) {
 if (commands[i].pattern && !commands[i].dontAddCommandList) {
-menu[commands[i].category] += `${config.PREFIX}${commands[i].pattern}\n`;
+menu[commands[i].category] += `➺${commands[i].pattern}\n`;
  }
 }
 
