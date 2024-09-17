2caa1c9bc2357e6869ebdffd31c177c004c3a774
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-17 10:30:25 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3ef5623a33997cf948438.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR I AM ONLINE",
+MONGODB: process.env.MONGODB || "",
 };
