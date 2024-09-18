75970a3b8cf9b6a208ac7c3e3f1ca5935eb5e414
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-19 02:27:45 +0500

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -6,31 +6,9 @@ cmd({
   desc: "Convert image to sticker",
   category: "convert",
   filename: __filename
-}, async (conn, mek, m, {
-  from,
-  quoted,
-  body,
-  isCmd,
-  command,
-  args,
-  q,
-  isGroup,
-  sender,
-  senderNumber,
-  botNumber2,
-  botNumber,
-  pushname,
-  isMe,
-  isOwner,
-  groupMetadata,
-  groupName,
-  participants,
-  groupAdmins,
-  isBotAdmins,
-  isAdmins,
-  reply
-}) => {
-  try {
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
     if (!mek.quoted || !mek.quoted.image) return reply("Reply to an image!");
     const imageBuffer = await downloadMediaMessage(mek.quoted);
     const image = await Jimp.read(imageBuffer);
