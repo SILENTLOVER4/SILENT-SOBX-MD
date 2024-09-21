789c0832003c1312e99935dbd4cf39d04d459532
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-21 08:41:59 +0500

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,6 +11,13 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const config = await readEnv();
+const botInfo = `
+      • Bot Name: ${config.BOT_NAME}
+      • Bot Version: ${config.BOT_VERSION}
+      • Uptime: ${process.uptime()} seconds
+      • Node.js Version: ${process.version}
+      • Platform: ${process.platform}
+    `;
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
