d2e777e8e3b94be95d98725d0b5be24b4deaba9d
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-23 00:44:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,6 @@ const participants = isGroup ? await groupMetadata.participants : ''
 const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
 const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
 const isAdmins = isGroup ? groupAdmins.includes(sender) : false
-const isReact = m.message.reactionMessage ? true : false
 const reply = (teks) => {
 conn.sendMessage(from, { text: teks }, { quoted: mek })
 }
