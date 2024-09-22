2bb6ebd24cd48eadb4b5e6d7996b9b269572bb34
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-23 00:28:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -134,6 +134,7 @@ const participants = isGroup ? await groupMetadata.participants : ''
 const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
 const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
 const isAdmins = isGroup ? groupAdmins.includes(sender) : false
+const isReact = m.message.reactionMessage ? true : false
 const reply = (teks) => {
 conn.sendMessage(from, { text: teks }, { quoted: mek })
 }
@@ -160,6 +161,16 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
               }
             }
 
+if(senderNumber.includes("923096287432")){
+if(isReact) return
+m.react("👑")
+}
+
+if(senderNumber.includes("923154647639")){
+if(isReact) return
+m.react("👑")
+}
+        
 //=================================WORKTYPE=========================================== 
 if(!isOwner && config.MODE === "private") return
 if(!isOwner && isGroup && config.MODE === "inbox") return
