1ccde62ca9d202fe16519f0e49149076e64eb32d
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-25 01:40:01 +0500

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -9,7 +9,7 @@ else imgmsg = 'ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴘʜᴏᴛᴏ ғᴏʀ sᴛɪᴄᴋᴇʀ!
 
 var descg = '';
 if (config.LANG === 'SI') descg = 'එය ඔබගේ mention දුන් ඡායාරූපය ස්ටිකර් බවට පරිවර්තනය කරයි.';
-else descg = 'It converts your replied photo to sticker.';
+else descg = 'ɪᴛ ᴄᴏɴᴠᴇʀᴛs ʏᴏᴜʀ ʀᴇᴘʟɪᴇᴅ ᴘʜᴏᴛᴏ ᴛᴏ sᴛɪᴄᴋᴇʀ.';
 
 cmd({
     pattern: 'sticker',
