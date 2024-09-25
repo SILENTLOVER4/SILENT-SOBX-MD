cfa63e15ee80d3623860ae180d5d48bf215b38c8
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-25 22:20:00 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -12,6 +12,21 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const config = await readEnv();
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `➺${commands[i].pattern}\n`;
+ }
+}
+
 let madeMenu = `❁ ════ ❃•◯•❃ ════ ❁
 
 *⇆ ʜɪɪ ᴍʏ ᴅᴇᴀʀ ғʀɪᴇɴᴅ ⇆*
