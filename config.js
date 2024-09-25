49bba49ff60369a9f55b9d0f1f3a6080a8434fb8
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-25 12:44:29 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "enter your session",
 MONGODB: process.env.MONGODB || "enter mongodb url",
 };
