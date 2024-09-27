43aa4b66ffd41f4d5ca46f09f54a13c5a6b09a96
Ɗᴀʀᴋ Ɗєνιʟ 👑🇵🇰🖤 <umarofficial404@gmail.com>
2024-09-27 19:42:03 +0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,174 +1,93 @@
-const {readEnv} = require('../lib/database')
-const {cmd , commands} = require('../command')
+const { cmd, commands } = require('../command');
+const config = require('../config')
 
 cmd({
-
-    pattern: "menu",
-
-    react: "🛸",
-
-    alias: ["panel","list","commands"],
-
-    desc: "Get bot\'s command list.",
-
-    category: "main",
-
-    use: '.menu',
-
+    pattern: "downmenu",
+    react: "⬇👨‍💻",
+    dontAddCommandList: true,
     filename: __filename
-
 },
-
-async(conn, mek, m,{from, l, quoted, body, isCmd, umarmd, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-const config = await readEnv();
-let madeMenu = `❁ ════ ❃•◯•❃ ════ ❁
+let menuc = `*● ══════════════ ●*
 
-*⇆ ʜɪɪ ᴍʏ ᴅᴇᴀʀ ғʀɪᴇɴᴅ ⇆*
+   *UMAR Ai DOWNLOAD COMMANDS MENU*\n\n`
+for (let i=0;i<commands.length;i++) { 
+if(commands[i].category === 'download'){
+  if(!commands[i].dontAddCommandList){
+menuc += `*📍➣Command :* ${commands[i].pattern}
+*📃➣Desc :* ${commands[i].desc}
+*⌛➣Use:* ${commands[i].use}\n\n`
+}}};
 
-     *${pushname}*
-     
-❁ ════ ❃•◯•❃ ════ ❁
-
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━
-      *ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ sɪʟᴇɴᴛ-sᴏʙx-ᴍᴅ ғᴜʟʟ ᴄᴏᴍᴍᴀɴᴅ ʟɪsᴛ*
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-*ᴄʀᴇᴀᴛᴇᴅ ʙʏ sɪʟᴇɴᴛ ʟᴏᴠᴇʀ⁴³²👨🏻‍💻*
-
-
-*╭──❮ DOWNLOAD COMMANDS ❯*
-│
-│📖 COMMAND: .play
-│ℹ️ Download Audio from yt
-│ 
-│📖 COMMAND: .song
-│ℹ️ Download song from yt
-│ 
-│📖 COMMAND: .apk
-│ℹ️ Download apk from playstore
-│ 
-│📖 COMMAND: .video
-│ℹ️ Download video from yt
-│ 
-│📖 COMMAND: .fb
-│ℹ️ Download  video from fb
-│ 
-│📖 COMMAND: .tk
-│ℹ️ Download video from tiktok
-│ 
-│📖 COMMAND: .ig
-│ℹ️ Download video from ig
-│ 
-│📖 COMMAND: .gdrive
-│ℹ️ Download drive files
-│ 
-│📖 COMMAND: .wamod
-│ℹ️ Download wamod apk
-│
-│📖 COMMAND: .img
-│ℹ️ Download image
-│
-│📖 COMMAND: .xvideo
-│ℹ️ Download xxx video
-╰────────────⦁ 
-
-*╭──❮ SEARCH COMMANDS ❯*
-│
-│📖 COMMAND: .yts
-│ℹ️ Serch videos from yt
-╰────────────⦁  
-
-*╭──❮‍ MAIN COMMANDS ❯*
-│
-│📖 COMMAND: .alive
-│ℹ️ Check online or not
-│  
-│📖 COMMAND: .ping
-│ℹ️ Check bot speed
-│  
-│📖 COMMAND: .menu
-│ℹ️ Nero main menu
-│
-│📖 COMMAND: .menu2
-│ℹ️ Nero main menu2
-│ 
-│📖 COMMAND: .ai
-│ℹ️ chat with ai bot
-│
-│📖 COMMAND: .system
-│ℹ️ check bot systems
-│
-│📖 COMMAND: .owner
-│ℹ️ get owner info
-│ 
-│📖 COMMAND: .status
-│ℹ️ check bot runtime
-╰────────────⦁
-
-*╭──❮ OTHER COMMANDS ❯*
-│
-│📖 COMMAND: .hirunews/news
-│ℹ️ Get news result for life
-│ 
-│📖 COMMAND: .wabeta
-│ℹ️ Get whatsapp beta news
-│
-│📖 COMMAND: .sitech
-│ℹ️ Get tech news
-│ 
-│📖 COMMAND: .nasa
-│ℹ️ Get nasa news
-╰────────────⦁
-
-*╭──❮ GROUP COMMANDS ❯*
-│
-│📖 COMMAND: .mute
-│ℹ️ Mute group
-│
-│📖 COMMAND: .unmute
-│ℹ️ Unmute group
-│
-│📖 COMMAND: .left
-│ℹ️ left group
-│
-│📖 COMMAND: .jid
-│ℹ️ group jid
-╰────────────⦁
-
-*╭──❮ OWNER COMMANDS ❯*
-│
-│📖 COMMAND: .update
-│ℹ️ update bot velue 
-│
-│📖 COMMAND: .restart 
-│ℹ️ restart your bot
-╰────────────⦁
-
-*╭──❮ CONVERT COMMANDS ❯*
-│
-│📖 COMMAND: .sticker
-│ℹ️ convert photo to sticker
-╰────────────⦁
+let generatebutton = [{
+    buttonId: `${prefix}sc`,
+    buttonText: {
+        displayText: 'ʙᴏᴛ ꜱᴄʀɪᴘᴛ'
+    },
+    type: 1
+  },{
+    buttonId: `${prefix}ping`,
+    buttonText: {
+        displayText: 'ᴜᴍᴀʀ ᴀɪ ꜱᴘᴇᴇᴅ'
+    },
+    type: 1
+  }]
+  let buttonMessaged = {
+    image: { url: `https://i.imgur.com/9WgnDo0.jpeg` },
+    caption: menuc,
+    footer: `ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴜᴍᴀʀ ᴀɪ ʙʏ ᴅᴀʀᴋ ᴅᴇᴠɪʟ`,
+    headerType: 4,
+    buttons: generatebutton
+  };
+  return await conn.buttonMessage(from, buttonMessaged, mek);
+} catch (e) {
+  reply('*ERROR !!*')
+  l(e)
+}
+})
 
 
+cmd({
+    pattern: "searchmenu",
+    react: "👨‍💻",
+    dontAddCommandList: true,
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menuc = `*● ══════════════ ●*
 
-┏━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-> *https://github.com/SILENTLOVER4/SILENT-SOBX-MD*
-
-┗━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ sɪʟᴇɴᴛ_ʟᴏᴠᴇʀ⁴³²*
-
-╰━❁ ═══ ❃•⇆•❃ ═══ ❁━╯
-`
-
-await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
-
-}catch(e){
-console.log(e)
-reply(`${e}`)
+   *SILENT Ai SEARCH COMMANDS MENU*\n\n`
+for (let i=0;i<commands.length;i++) { 
+if(commands[i].category === 'search'){
+  if(!commands[i].dontAddCommandList){
+menuc += `*📍➣Command :* ${commands[i].pattern}
+*📃➣Desc :* ${commands[i].desc}
+*⌛➣Use:* ${commands[i].use}\n\n`
+}}};
+let generatebutton = [{
+    buttonId: `${prefix}sc`,
+    buttonText: {
+        displayText: 'ʙᴏᴛ ꜱᴄʀɪᴘᴛ'
+    },
+    type: 1
+  },{
+    buttonId: `${prefix}ping`,
+    buttonText: {
+        displayText: 'SILENT-SOBX-MD SPEED'
+    },
+    type: 1
+  }]
+    image: { url: `https://i.imgur.com/9WgnDo0.jpg` },
+    caption: menuc,
+    footer: `ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴜᴍᴀʀ ᴀɪ ʙʏ ᴅᴀʀᴋ ᴅᴇᴠɪʟ`,
+    headerType: 4,
+    buttons: generatebutton
+  };
+  return await conn.buttonMessage(from, buttonMessaged, mek);
+} catch (e) {
+  reply('*ERROR !!*')
+  l(e)
 }
 })
