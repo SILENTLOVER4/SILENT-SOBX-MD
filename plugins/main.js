c5683ef4b79e4a70c31f19977d9c1a39ff39f856
Ɗᴀʀᴋ Ɗєνιʟ 👑🇵🇰🖤 <umarofficial404@gmail.com>
2024-09-27 19:44:25 +0500

    Create main.js

---
+++ plugins/main.js
@@ -0,0 +1,234 @@
+const config = require('../config')
+const os = require('os')
+const { cmd, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+
+
+cmd({
+    pattern: "alive",
+    react: "👨‍💻",
+    alias: ["online","test","bot"],
+    desc: "Check bot online or no.",
+    category: "main",
+    use: '.alive',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+	var msg = mek
+if(os.hostname().length == 12 ) hostname = 'replit'
+else if(os.hostname().length == 36) hostname = 'heroku'
+else if(os.hostname().length == 8) hostname = 'koyeb'
+else hostname = os.hostname()
+let monspace ='```'
+let monspacenew ='`'
+if(config.ALIVE === "default") {
+ const sections = [
+    {
+	title: "",
+	rows: [
+	    {title: "1", rowId: prefix + 'menu' , description: 'COMMANDS MENU'},
+	    {title: "2", rowId: prefix + 'ping' , description: 'UMAR Ai SPEED'},
+
+	]
+    } 
+]
+const listMessage = {
+  caption: `${monspace}👋 කොහිමද ${pushname} I'm alive now${monspace}
+    
+*🚀Version:* ${require("../package.json").version}
+*⌛Memory:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*🕒Runtime:* ${runtime(process.uptime())}
+*📍Platform:* ${hostname}
+
+🐼This is the result of our teams hard work and our technical cybers team owns the bots rights and code rights. Therefore, you have no chance to change and submit our bot under any circumstances And 100 Commands And logo, thumbnail,banner Maker Commands Ai Chatbot feathers On Our Bot
+                    
+*🌻Have A Nice Day..*🌻`,
+  image : { url : config.LOGO} ,
+  footer: config.FOOTER,
+  buttonText: "🔢 Reply below number,",
+  sections,
+  contextInfo: {
+				
+				externalAdReply: { 
+					title: 'U M A R  Ai BOT',
+					body: 'ᴀɴ ᴜꜱᴇʀ ʙᴏᴛ ꜰᴏʀ ᴡʜᴀᴛꜱᴀᴘᴘ',
+					mediaType: 1,
+					sourceUrl: "" ,
+          thumbnailUrl: 'https://i.imgur.com/9WgnDo0.jpeg' ,
+					renderLargerThumbnail: false,
+          showAdAttribution: true
+         }}	
+}
+
+return await conn.replyList(from, listMessage ,{ quoted : msg }) 
+}
+else {
+  const sections = [
+    {
+	title: "",
+	rows: [
+	    {title: "1", rowId: prefix + 'menu' , description: 'COMMANDS MENU'},
+	    {title: "2", rowId: prefix + 'ping' , description: 'UMAR Ai SPEED'} ,
+
+	]
+    } 
+]
+const listMessage = {
+  caption: config.ALIVE,
+  image : { url : config.LOGO} ,
+  footer: config.FOOTER,
+  buttonText: "🔢 Reply below number,",
+  sections,
+  contextInfo: {
+				
+				externalAdReply: { 
+					title: 'U M A R  Ai BOT',
+					body: 'ᴀɴ ᴜꜱᴇʀ ʙᴏᴛ ꜰᴏʀ ᴡʜᴀᴛꜱᴀᴘᴘ',
+					mediaType: 1,
+					sourceUrl: "" ,
+          thumbnailUrl: 'https://i.imgur.com/9WgnDo0.jpeg' ,
+					renderLargerThumbnail: false,
+          showAdAttribution: true
+         }}	
+}
+
+return await conn.replyList(from, listMessage ,{ quoted : msg })
+}
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+})
+
+
+
+cmd({
+    pattern: "ping",
+    react: "📟",
+    alias: ["speed"],
+    desc: "Check bot\'s ping",
+    category: "main",
+    use: '.ping',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+var inital = new Date().getTime();
+let ping = await conn.sendMessage(from , { text: '```Pinging To Umar Ai!!!```'  }, { quoted: mek } )
+var final = new Date().getTime();
+return await conn.edit(ping, '*Pong*\n *' + (final - inital) + ' ms* ' )
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+})
+
+
+cmd({
+  pattern: "menu",
+  react: "👨‍💻",
+  alias: ["panel","list","commands"],
+  desc: "Get bot\'s command list.",
+  category: "main",
+  use: '.menu',
+  filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+	var msg = mek
+if(os.hostname().length == 12 ) hostname = 'replit'
+else if(os.hostname().length == 36) hostname = 'heroku'
+else if(os.hostname().length == 8) hostname = 'koyeb'
+else hostname = os.hostname()
+let monspace ='```'
+let monspacenew ='`'
+if(config.ALIVE === "default"){
+ const sections = [
+    {
+	title: "",
+	rows: [
+	    {title: "1", rowId: prefix + 'downmenu' , description: 'Down Commands'},
+	    {title: "2", rowId: prefix + 'searchmenu' , description: 'Search Commands'},
+	    {title: "3", rowId: prefix + 'convertmenu' , description: 'Convert Commands'}, 
+	    {title: "4", rowId: prefix + 'logomenu' , description: 'Logo Commands'},
+	    {title: "5", rowId: prefix + 'ownermenu' , description: 'Owner Commands'},
+	    {title: "6", rowId: prefix + 'adminmenu' , description: 'Admin Commands'},
+	    {title: "7", rowId: prefix + 'othermenu' , description: 'Other commands'},
+
+	]
+    } 
+]
+const listMessage = {
+  caption: `👋 ❤කොහොමද ${pushname} I'm alive now
+    
+*👾 UMAR Ai commands menu...*
+  
+ *🚀Version:* ${require("../package.json").version}
+ *⌛Memory:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+ *🕒Runtime:* ${runtime(process.uptime())}
+ *📍Platform:* ${hostname}`,
+  image : { url : config.LOGO} ,
+  footer: config.FOOTER,
+  buttonText: "🔢 Reply below number,",
+  sections,
+  contextInfo: {
+				
+				externalAdReply: { 
+					title: 'U M A R  Ai BOT',
+					body: 'ᴀɴ ᴜꜱᴇʀ ʙᴏᴛ ꜰᴏʀ ᴡʜᴀᴛꜱᴀᴘᴘ',
+					mediaType: 1,
+					sourceUrl: "" ,
+          thumbnailUrl: 'https://i.imgur.com/9WgnDo0.jpeg' ,
+					renderLargerThumbnail: false,
+          showAdAttribution: true
+         }}	
+}
+
+return await conn.replyList(from, listMessage ,{ quoted : msg }) 
+}
+else {
+  const sections = [
+    {
+	title: "",
+	rows: [
+	    {title: "1", rowId: prefix + 'downmenu' , description: 'Down Commands'},
+	    {title: "2", rowId: prefix + 'searchmenu' , description: 'Search Commands'},
+	    {title: "3", rowId: prefix + 'convertmenu' , description: 'Convert Commands'}, 
+	    {title: "4", rowId: prefix + 'logomenu' , description: 'Logo Commands'},
+	    {title: "5", rowId: prefix + 'ownermenu' , description: 'Owner Commands'},
+	    {title: "6", rowId: prefix + 'adminmenu' , description: 'Admin Commands'},
+	    {title: "7", rowId: prefix + 'othermenu' , description: 'Other commands'},
+
+
+	]
+    } 
+]
+const listMessage = {
+  caption: config.ALIVE,
+  image : { url : config.LOGO} ,
+  footer: config.FOOTER,
+  buttonText: "🔢 Reply you select number,",
+  sections,
+  contextInfo: {
+				
+				externalAdReply: { 
+					title: 'U M A R  Ai BOT ',
+					body: 'ᴀɴ ᴜꜱᴇʀ ʙᴏᴛ ꜰᴏʀ ᴡʜᴀᴛꜱᴀᴘᴘ',
+					mediaType: 1,
+					sourceUrl: "" ,
+          thumbnailUrl: 'https://i.imgur.com/9WgnDo0.jpeg' ,
+					renderLargerThumbnail: false,
+          showAdAttribution: true
+         }}	
+}
+
+return await conn.replyList(from, listMessage ,{ quoted : msg })
+}
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+})
+
+	
