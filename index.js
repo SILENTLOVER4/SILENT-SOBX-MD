f10cf393589e9a9f15deb4fc1c9638166c9dc60a
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-28 20:14:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -110,7 +110,8 @@ if (!mek.message) return
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
 await conn.readMessages([mek.key])
-await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:up},{quoted:mek})
+let up = `hii 🙈`
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/2a06381b260c3f096a612.jpg` }, caption: up })
 }
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
