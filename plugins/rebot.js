e725b0eaaed0549ec9cdfab04550ebb452130d66
Ɗᴀʀᴋ Ɗєνιʟ 👑🇵🇰🖤 <umarofficial404@gmail.com>
2024-09-28 21:39:11 +0500

    Create rebot.js

---
+++ plugins/rebot.js
@@ -0,0 +1,22 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const {sleep} = require('../lib/functions')
+
+cmd({
+    pattern: "rebot",
+    desc: "restart the bot UMAR🔥",
+    category: "owner",
+    react: "😎",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const {exec} = require("child_process")
+reply("restarting...")
+await sleep(1500)
+exec("pm2 restart all")
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
