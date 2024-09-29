f48546795f3ad990b7cddf7a3bf5daf3a711ea93
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-29 09:48:03 +0500

    Update gpass.js

--- plugins/gpass.js
+++ plugins/gpass.js
@@ -26,7 +26,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         };
 
         const password = generatePassword(length);
-        const message = `🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n *Umar Rehman*`;
+        const message = `🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n *BY SILENT-SOBX-MD*`;
 
         // Send initial notification message
         await conn.sendMessage(from, { text: message }, { quoted: mek });
