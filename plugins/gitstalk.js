b546d63cff22c8acf2c58adfc6f390bcd3190bd5
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-29 09:46:01 +0500

    Update gitstalk.js

--- plugins/gitstalk.js
+++ plugins/gitstalk.js
@@ -20,7 +20,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        let userInfo = `     👨‍💻*Umar GitStalk*👨‍💻
+        let userInfo = `     👨‍💻*SILENT-SOBX-MD GITSTALK*👨‍💻
         
 👤 *ᴜꜱᴇʀ ɴᴀᴍᴇ*: ${data.name || data.login}
 
@@ -38,7 +38,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
 🔭 *ᴘᴜʙʟɪᴄ ɢɪꜱᴛꜱ*: ${data.public_gists}
 
-*Made With ♥ By Umar*
+*MADE ♥ BY SILENT-SOBX-MD*
 `;
 
         await conn.sendMessage(from, { image: { url: data.avatar_url }, caption: userInfo }, { quoted: mek });
