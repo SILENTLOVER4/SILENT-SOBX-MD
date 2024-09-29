c4168d1edc135775e6b8ea311fe9a624698e377a
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-29 18:48:32 +0500

    Update dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: 'SILENT-SOBX-MD DOWNLOAD📂DOG PICS\n\n\n > *BY SILENTLOVER432*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: 'SILENT-SOBX-MD DOWNLOAD📂DOG PICS\n\n\n> *BY SILENTLOVER432*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`Error Fetching Dog Image🤕: ${e.message}`);
