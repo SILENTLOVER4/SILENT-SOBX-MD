06aa32ef93bb861b31c8571ddc2d5203cfbe2520
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-09-29 22:58:07 +0500

    Update kick.js

--- plugins/kick.js
+++ plugins/kick.js
@@ -8,28 +8,54 @@ cmd({
   desc: "kick member from group.", 
   category: "group", 
   filename: __filename,
-},
-async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
+}, 
+async (conn, mek, m, { 
+  from, 
+  l, 
+  quoted, 
+  body, 
+  isCmd, 
+  command, 
+  args, 
+  q, 
+  isGroup, 
+  sender, 
+  senderNumber, 
+  botNumber2, 
+  botNumber, 
+  pushname, 
+  isMe, 
+  isOwner, 
+  groupMetadata, 
+  groupName, 
+  participants, 
+  isItzcp, 
+  groupAdmins, 
+  isBotAdmins, 
+  isAdmins, 
+  reply
+}) => {
+  try {
     if (!isOwner || !isAdmins) return;
     if (!m.isGroup) return reply(mg.onlygroup);
     if (!isBotAdmins) return reply(mg.needbotadmins);
     if (args.length === 0) return reply('Tag or mention member to kick.');
     const mention = mentionedJid(args[0]);
-    await conn.groupParticipantsUpdate(m.chat, [mention], 'remove');
-    const mass = await conn.sendMessage(m.chat, { 
+    await conn.groupParticipantsUpdate(mention, 'remove');
+    const mass = await conn.sendMessage(mention, { 
       text: `*${mention.replace(/@/g, '@')} KICKED BY SILENT-SOBX-MD*`
     }, { 
       quoted: mek 
     });
-    return await conn.sendMessage(m.chat, { 
+    return await conn.sendMessage(mention, { 
       react: { 
         text: '', 
         key: mass.key 
       } 
     });
   } catch (e) {
-    console.log(e);
-    reply('*PLEASE GIVE ME A ADMIN ROLE')
+    console.log('Error Message:', e.message)
+    console.log('Error Stack:', e.stack)
+    reply('An error occurred while kicking member. Check console logs.')
   }
 })
