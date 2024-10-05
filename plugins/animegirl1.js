b4fe68ed2c84081e8803d0b3d83ddaccdb99d770
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-10-05 10:49:32 +0500

    Update animegirl1.js

--- plugins/animegirl1.js
+++ plugins/animegirl1.js
@@ -1,24 +1,3 @@
-//   █▀▀▀ █  █ █  █ █▀▀█ █▀▀▀      █▀▄▀█ █▀▀▄ 
-//   ▀▀▀█ █  █ █▀▀█ █▀▀█ ▀▀▀█  ▀▀  █ ▀ █ █  █
-//   ▀▀▀▀ ▀▀▀▀ ▀  ▀ ▀  ▀ ▀▀▀▀      ▀   ▀ ▀▀▀
-
-
-
-// * Project name - SUHAS- MD 
-// * Author - Suhas Pathsindu
-// * Team - Suhas Bro 
-// * Version - V2
-
-// World best and powerfull whatsapp user bot in Sri lanka
-//🧬©ꜱᴜʜᴀꜱ-ᴍᴅ ʙʏ ꜱᴜᴀʜꜱ ᴘᴀᴛʜꜱɪɴᴅᴜッ
-
-
-
-
-
-
-
-
 const axios = require('axios');
 const { cmd, commands } = require('../command');
 
@@ -35,10 +14,9 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SUHAS-MD Random Anime Girl Images* 👸\n\n\n *🧬©ꜱᴜʜᴀꜱ-ᴍᴅ ʙʏ ꜱᴜᴀʜꜱ ᴘᴀᴛʜꜱɪɴᴅᴜッ🇱🇰*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SILENT-SOBX-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SILENT-SOBX-MD BY SILENTLOVER432*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
     }
 });
-
