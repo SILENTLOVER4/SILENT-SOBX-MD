fbef5a651388439dd2184802997808cd6dc83bb6
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-10-06 09:22:34 +0500

    Add files via upload

---
+++ plugins/gpt.js
@@ -0,0 +1,41 @@
+//   █▀▀▀ █  █ █  █ █▀▀█ █▀▀▀      █▀▄▀█ █▀▀▄ 
+//   ▀▀▀█ █  █ █▀▀█ █▀▀█ ▀▀▀█  ▀▀  █ ▀ █ █  █
+//   ▀▀▀▀ ▀▀▀▀ ▀  ▀ ▀  ▀ ▀▀▀▀      ▀   ▀ ▀▀▀
+
+
+
+// * Project name - SUHAS- MD 
+// * Author - Suhas Pathsindu
+// * Team - Suhas Bro 
+// * Version - V2
+
+// World best and powerfull whatsapp user bot in Sri lanka
+//🧬©ꜱᴜʜᴀꜱ-ᴍᴅ ʙʏ ꜱᴜᴀʜꜱ ᴘᴀᴛʜꜱɪɴᴅᴜッ
+
+
+
+
+
+
+
+
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+cmd({
+    pattern: "gpt",
+    desc: "ai",
+    category: "main",
+    react: "🧠",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
+reply(`${data.data}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
