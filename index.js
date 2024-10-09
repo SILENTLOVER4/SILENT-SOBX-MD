bb2b82eacc64d36d3d36485c01df55dca450a117
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-10-09 15:21:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,8 @@ const qrcode = require('qrcode-terminal')
 const StickersTypes = require('wa-sticker-formatter')
 const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
-const axios = require('axios')
+const axios require('axios')
 const { File } = require('megajs')
-const bodyparser = require('body-parser')
-const expressjson = require('express-json')
-const mongoose = require('mongoose')
 
 const ownerNumber = ['923096287432']
 
