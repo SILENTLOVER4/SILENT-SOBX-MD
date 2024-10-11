5feed06aaffc29710f69bb7a454a0f498559c473
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-10-11 12:52:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -112,6 +112,8 @@ if (!mek.message) return
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
 await conn.readMessages([mek.key])
+const reply = 'Hay, your status seen successfully!';
+await conn.sendMessage(mek.key.remoteJid, { text: reply });
 }
 const m = sms(conn, mek)
 const type = getContentType(mek.message)
