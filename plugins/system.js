050b62045a04c97df09e3786577e86fe9a3a0043
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-10-15 10:03:41 +0500

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -5,17 +5,22 @@ const {runtime} = require('../lib/functions')
 cmd({
     pattern: "system",
     react: "♠️",
-    alias: ["info","status"],
+    alias: ["uptime","status"],
     desc: "cheack uptime",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `*Uptime:*  ${runtime(process.uptime())}
-*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*HostName:* ${os.hostname()}
-*Owner:* SILENT LOVER⁴³²
+let status = `*SILENT-SOBX-MD UPTIME LIST*
+
+*UPTIME:*  ${runtime(process.uptime())}
+
+*RAM USAGE:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+
+*HOSTNAME:* ${os.hostname()}
+
+*OWNER:* *SILENT LOVER⁴³²*
 `
 return reply(`${status}`)
 }catch(e){
