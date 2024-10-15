05724e106d37b397b5e7a1f5c233ed346d4d3d05
SILENT LOVER 432 <141915920+SILENTLOVER4@users.noreply.github.com>
2024-10-15 11:31:46 +0500

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -5,7 +5,7 @@ const {runtime} = require('../lib/functions')
 cmd({
     pattern: "system",
     react: "♠️",
-    alias: ["uptime","status"],
+    alias: ["uptime","status","runtime"],
     desc: "cheack uptime",
     category: "main",
     filename: __filename
